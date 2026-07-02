f5df5ba89805956411b73ddd5f3886fb0c2ccd92
juliatmuller <julia_t_muller@estudante.sesisenai.org.br>
2026-07-02 08:35:47 -0300

    add - arrumei a ex1

--- ex_01.php
+++ ex_01.php
@@ -3,15 +3,17 @@
 
 function calcularFormula($x,$y)
 {
-    if (($x+$y)= 0 ){
+    if (($x + $y) == 0 ) {
         return "Não é possivel realizar a divisão por zero.";
     }
-    $resultado = (pow($x,2)+ pow($y,2))/ ($x+$y);
+
+    $resultado = (pow($x,2) + pow($y,2)) / ($x + $y);
+
     return $resultado;
 }
 
 $x = 10;
-$y = 5
+$y = 5;
 
 echo "Valor de X: $x <br>";
 echo "Valor de Y: $y <br><br>";
