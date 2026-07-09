19ec6a676e978e3b9c30cb4c5ccf3c89272760ba
juliatmuller <julia_t_muller@estudante.sesisenai.org.br>
2026-07-09 07:21:51 -0300

    add - ex 04

--- ex_04.php
+++ ex_04.php
@@ -1,6 +1,16 @@
 <?php
 
-function gerarSenha($caracteres)
-{
-    
+function gerarSenha($tamanho = 20){
+
+    $caracteres = "0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ!@#$%&*";
+
+    $senha = "";
+
+    for($i = 0; $i < $tamanho; $i++){
+        $senha .= $caracteres[rand(0, strlen($caracteres) - 1)];
+    }
+
+    return $senha;
 }
+
+echo "Senha gerada: " . gerarSenha();
